aa9605354f0d649bc27aba780a97cae9b0a63305
Norah Say <norahsay@gmail.com>
2019-05-10 11:05:55 -0700

    Added resuable components to common folder

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Header } from './components/common'; 
 
 
 class App extends Component {
   render() {
     return (
-      <View>
-        <Text>Welcome to OutManual</Text>
-      </View>
+      <Header headerText="Log In" />
     );
   }
 }
